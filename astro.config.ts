7f5ba3586121d6ae7d1fc35b1ab5a93bb529e633
chaoleiwang <1905943612@qq.com>
2026-04-12 22:33:55 +0800

    astro 临时提交

--- astro.config.ts
+++ astro.config.ts
@@ -3,11 +3,13 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
+import react from '@astrojs/react';
+
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	site: 'https://luckdiy.github.io',
+	integrations: [mdx(), sitemap(),react()],
 	fonts: [
 		{
 			provider: fontProviders.local(),
